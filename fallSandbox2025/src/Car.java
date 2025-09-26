959bde40abdb2f3a7d71babf721125946d8e0a5c
jrdelt <cesardeltorojr@gmail.com>
2025-09-26 13:33:23 -0700

    Added a java project to the repo

---
+++ fallSandbox2025/src/Car.java
@@ -0,0 +1,21 @@
+
+// Visibility (Public) - Every file can access this file.
+
+public class Car {
+    
+    //Variable: states of a variable.
+    private boolean working; //Accessible only by this class
+    private boolean moving; //Accessible only by this class
+    private boolean totalled; //Accessible only by this class
+    private double speed; //Accessible only by this class
+    private int numberWheels; //Accessible only by this class
+
+    //Method: behavior of a class.
+    public int move(){
+        int x = 12;
+        return x;
+    }
+
+
+
+}
